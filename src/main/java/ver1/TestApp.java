12a91f35ae05dfe94569b2f9bd3fd98ed1cad287
Alex Lenzi <lenzi-alex@libero.it>
2022-05-01 00:03:10 +0200

    ver 1.0

--- src/main/java/ver1/TestApp.java
+++ src/main/java/ver1/TestApp.java
@@ -12,14 +12,14 @@ public class TestApp {
             init.add(new Studente(2, "Nicola", "Bicocchi"));
             Studente.saveToCSV(init, "save.csv");
         } catch (IOException e) {
-            System.out.println("Error! Save to CVS failed");
+            System.out.println("Error! Save to CSV failed");
         }
         // LOAD from CSV file
         try {
             ArrayList<Studente> students = Storage.loadFromCSV("load.csv", new Studente(), Studente.class);
             System.out.println(students);
         } catch (IOException e) {
-            System.out.println("Error! Load from CVS failed");
+            System.out.println("Error! Load from CSV failed");
         }
     }
 }
